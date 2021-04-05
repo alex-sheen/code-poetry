301d2d9a0a938344fcaf84f88ef6125e8f14286a
Alex <alexsheen@uchicago.edu>
2021-04-04 23:37:02 -0500

    added code poetry reply

---
+++ sheen_reply.c
@@ -0,0 +1,24 @@
+#include <latin.translation.h>
+
+/* Catullus | 101 */
+
+//Alas, poor brother, unfairly taken away from me,
+heu .miser indigne /*frater*/ adempte "mihi"
+
+//now in the meantime, nevertheless, receive these things which in the ancient custom of our fathers
+nunc tamen interea haec, prisco quae more parentum
+
+//having been handed down, a sad tribute to the funeral rites
+tradita sunt .tristi munere ad /*inferias*/
+
+//receive them, dripping much with a brother's tears
+accipe "fraterno" <multum> -        -      -   -   o    -       -
+-   -     -      -    <manantia> -      -   -    o * o    -  o     -   -
+  -      -   -      -     -     <fletu>      -  o * * o  - o * o  -   o  -    -
+  -  -    -   -    -   -    -   -   -     -    - o o o  - o * * o   o * o      -
+-   -    -   -  -    -   -    -   -    -   -   -   -   -   o o o   o * * o  -
+  -    -     -     -  -    -    -   -   -     -     -    -    -  -  o o o  -
+//And forever, brother, hail and farewell.
+atque in .perpetuum
+                /*frater*/
+                    "ave atque vale"
